4a80fab8d9ecd4dc786632841d256f3e189f2667
Brandon Denton <bdenton418@gmail.com>
2017-05-09 14:07:50 -0400

    not sure what to port so I've set up manual auth for now

---
+++ backend/routes/login.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+
+/*
+|--------------------------------------------------------------------------
+| Login Route
+|--------------------------------------------------------------------------
+|
+| This route handles authentication for a user of the plugin. *(Facebook
+| Authentication is handled elsewhere in this directory)
+|
+*/
+
+Route::get('/login', function () {
+    if (Auth::attempt(['email' => $email, 'password' => $password])) {
+        return redirect()->intended('welcome');     
+    }
+});
